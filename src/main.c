22309d81ea9854600b51fde5ce34a6ef36d264c2
youssef0408 <othmaniyoussef97@gmail.com>
2024-05-23 20:01:11 -0400

    WIP fixing leaks while bad elements

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yothmani <yothmani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 00:40:51 by joe_jam           #+#    #+#             */
-/*   Updated: 2024/05/23 11:42:23 by yothmani         ###   ########.fr       */
+/*   Updated: 2024/05/23 12:53:39 by yothmani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,7 @@ int	main(int argc, char **argv)
 		return (1);
 	init_map_struct(&game.map, fd);
 	if (parse(argc, argv, &game.map, &fd))
-	{
-		free_game(&game);
 		return (1);
-	}
 	close(fd);
 	if (init_game(&game))
 		return (1);
